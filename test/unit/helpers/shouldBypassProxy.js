524b32cce9ca479499d89e45dffb44bae905f78a
Seal <info@sealsecurity.io>
2026-09-15 11:46:47 +0000

    CVE-2026-42043

--- test/unit/helpers/shouldBypassProxy.js
+++ test/unit/helpers/shouldBypassProxy.js
@@ -80,4 +80,53 @@ describe('helpers::shouldBypassProxy', function () {
     setNoProxy('*');
     assert.strictEqual(shouldBypassProxy('http://anything.example.com/'), true);
   });
+
+  it('should bypass proxy for 127.0.0.0/8 subnet when no_proxy contains 127.0.0.1 (GHSA-pmwg-cvhr-8vh7)', function () {
+    setNoProxy('localhost,127.0.0.1,::1');
+    assert.strictEqual(shouldBypassProxy('http://127.0.0.2:9191/secret'), true);
+    assert.strictEqual(shouldBypassProxy('http://127.0.0.100:9191/secret'), true);
+    assert.strictEqual(shouldBypassProxy('http://127.1.2.3:9191/secret'), true);
+    assert.strictEqual(shouldBypassProxy('http://127.255.255.254:9191/secret'), true);
+  });
+
+  it('should bypass proxy for 127.0.0.0/8 subnet when no_proxy contains localhost', function () {
+    setNoProxy('localhost');
+    assert.strictEqual(shouldBypassProxy('http://127.0.0.2:7777/'), true);
+    assert.strictEqual(shouldBypassProxy('http://127.1.2.3:7777/'), true);
+  });
+
+  it('should NOT bypass for non-loopback IPv4 addresses', function () {
+    setNoProxy('localhost,127.0.0.1,::1');
+    assert.strictEqual(shouldBypassProxy('http://128.0.0.1:9191/'), false);
+    assert.strictEqual(shouldBypassProxy('http://126.255.255.255:9191/'), false);
+    assert.strictEqual(shouldBypassProxy('http://10.0.0.1:9191/'), false);
+    assert.strictEqual(shouldBypassProxy('http://192.168.1.1:9191/'), false);
+  });
+
+  it('should bypass proxy for full-form IPv6 loopback 0:0:0:0:0:0:0:1', function () {
+    setNoProxy('localhost,127.0.0.1,::1');
+    assert.strictEqual(shouldBypassProxy('http://[0:0:0:0:0:0:0:1]:8080/'), true);
+  });
+
+  it('should bypass proxy for IPv4-mapped IPv6 loopback ::ffff:127.0.0.1', function () {
+    setNoProxy('localhost,127.0.0.1,::1');
+    assert.strictEqual(shouldBypassProxy('http://[::ffff:127.0.0.1]:8080/'), true);
+  });
+
+  it('should treat 127.x.x.x as cross-equivalent to localhost and ::1', function () {
+    setNoProxy('::1');
+    assert.strictEqual(shouldBypassProxy('http://127.0.0.5:7777/'), true);
+  });
+
+  it('should still respect explicit port mismatch on no_proxy entries', function () {
+    setNoProxy('127.0.0.1:8080');
+    assert.strictEqual(shouldBypassProxy('http://127.0.0.2:8080/'), true);
+    assert.strictEqual(shouldBypassProxy('http://127.0.0.2:9090/'), false);
+  });
+
+  it('should not bypass for hosts that merely contain 127 in other octets', function () {
+    setNoProxy('localhost,127.0.0.1,::1');
+    assert.strictEqual(shouldBypassProxy('http://10.0.0.127:8080/'), false);
+    assert.strictEqual(shouldBypassProxy('http://200.127.0.1:8080/'), false);
+  });
 });
