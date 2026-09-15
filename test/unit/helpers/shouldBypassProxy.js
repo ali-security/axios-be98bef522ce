70916a94ffff9a2755e6640e79d655238740af47
Seal <info@sealsecurity.io>
2026-09-15 11:46:47 +0000

    CVE-2026-42038

--- test/unit/helpers/shouldBypassProxy.js
+++ test/unit/helpers/shouldBypassProxy.js
@@ -39,6 +39,26 @@ describe('helpers::shouldBypassProxy', function () {
     assert.strictEqual(shouldBypassProxy('http://[::1]:8080/'), true);
   });
 
+  it('should bypass proxy for 127.0.0.1 when no_proxy contains localhost', function () {
+    setNoProxy('localhost');
+    assert.strictEqual(shouldBypassProxy('http://127.0.0.1:7777/'), true);
+  });
+
+  it('should bypass proxy for [::1] when no_proxy contains localhost', function () {
+    setNoProxy('localhost');
+    assert.strictEqual(shouldBypassProxy('http://[::1]:7777/'), true);
+  });
+
+  it('should bypass proxy for localhost when no_proxy contains 127.0.0.1', function () {
+    setNoProxy('127.0.0.1');
+    assert.strictEqual(shouldBypassProxy('http://localhost:7777/'), true);
+  });
+
+  it('should bypass proxy for localhost when no_proxy contains ::1', function () {
+    setNoProxy('::1');
+    assert.strictEqual(shouldBypassProxy('http://localhost:7777/'), true);
+  });
+
   it('should match wildcard and explicit ports', function () {
     setNoProxy('*.example.com,localhost:8080');
     assert.strictEqual(shouldBypassProxy('http://api.example.com/'), true);
